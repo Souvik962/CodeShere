62aa1c3939bfc4ae37f6376e5cc871f9f60d52c0
Souvik Mondal <140205356+Souvik962@users.noreply.github.com>
2025-12-01 12:20:42 +0530

    Add production CORS origin for socket server

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:5173",               // local development
+      "https://codeshere-main.onrender.com/",                         //"http://localhost:5173",               // local development
       process.env.CLIENT_URL || "*",         // production
     ],
     credentials: true,
