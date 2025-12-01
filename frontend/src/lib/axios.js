4a75550b29479515db1f170396ab5343c4f49d92
Souvik Mondal <140205356+Souvik962@users.noreply.github.com>
2025-12-01 11:40:44 +0530

    localhost update at axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://codeshere.onrender.com" : "/api",
+  baseURL: import.meta.env.MODE === "development" ?  "http://localhost:3000/api" : "/api",//"https://codeshere.onrender.com" : "/api",
   withCredentials: true,
   timeout: 30000, // 30 second timeout for email operations
 });
